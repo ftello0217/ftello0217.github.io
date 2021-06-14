35f7ecedef7223cc379d1cafb3a81d177b6772b2
ftello0217 <85448255+ftello0217@users.noreply.github.com>
2021-06-14 15:34:43 -0500

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -49,7 +49,7 @@ soundArray.push(wallSound); //add sound to list of sounds
 export var paddleSound = new soundFile("tap-resonant-4.mp3");
 soundArray.push(paddleSound);
 
-export var scoreSound = new soundFile("ocean_waves-4.mp3");
+export var scoreSound = new soundFile("ocean-waves-4.mp3");
 soundArray.push(scoreSound);
 
 export var ambientSound = new soundFile("Adventure_Meme-4.mp3");
